5b18a47cbabd5cf779e3bac9b0df36c957bc6eab
袁老三 <yhonglai@163.com>
2018-10-24 22:31:10 +0800

    完善架构

---
+++ Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User
+    {
+        public string UserName { get; set; } 
+        public string Password { get; set; }
+        public string Uid { get; set; }
+
+        public string NickName { get; set; }//昵称
+        public Image HeaderPicture { get; set; }//头像
+
+        public CookieContainer Cookies { get; set; }//Cookies
+
+    }
+}
