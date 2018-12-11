b59cf0622644c4e93b6d16986d97b643bb0c905b
袁老三 <yhonglai@163.com>
2018-12-11 22:40:45 +0800

    修改控件按钮显示文字

--- WIN/Controls/UserLoginControl.Designer.cs
+++ WIN/Controls/UserLoginControl.Designer.cs
@@ -35,6 +35,7 @@
             System.Windows.Forms.Label label5;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserLoginControl));
             this.skinGroupBox1 = new CCWin.SkinControl.SkinGroupBox();
+            this.buttonClean = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
             this.buttonStart = new System.Windows.Forms.Button();
             this.labelGrouCount = new System.Windows.Forms.Label();
@@ -43,7 +44,6 @@
             this.labelNowFansCount = new System.Windows.Forms.Label();
             this.labelLoginFansCount = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonClean = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
@@ -130,13 +130,23 @@
             this.skinGroupBox1.TitleRectBackColor = System.Drawing.SystemColors.Control;
             this.skinGroupBox1.TitleRoundStyle = CCWin.SkinClass.RoundStyle.All;
             // 
+            // buttonClean
+            // 
+            this.buttonClean.Location = new System.Drawing.Point(289, 57);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(80, 30);
+            this.buttonClean.TabIndex = 13;
+            this.buttonClean.Text = "一键清粉";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
             // buttonExit
             // 
             this.buttonExit.Location = new System.Drawing.Point(289, 94);
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(80, 30);
             this.buttonExit.TabIndex = 12;
-            this.buttonExit.Text = "退出";
+            this.buttonExit.Text = "退出登录";
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
@@ -146,7 +156,7 @@
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(80, 30);
             this.buttonStart.TabIndex = 11;
-            this.buttonStart.Text = "开始";
+            this.buttonStart.Text = "开始互粉";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
@@ -203,16 +213,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // buttonClean
-            // 
-            this.buttonClean.Location = new System.Drawing.Point(289, 57);
-            this.buttonClean.Name = "buttonClean";
-            this.buttonClean.Size = new System.Drawing.Size(80, 30);
-            this.buttonClean.TabIndex = 13;
-            this.buttonClean.Text = "一键清粉";
-            this.buttonClean.UseVisualStyleBackColor = true;
-            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
             // 
             // UserLoginControl
             // 
