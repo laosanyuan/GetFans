fb5f63e9151d0985ed6f2ada427fb735eb8b9943
yuanhonglai <yuanhonglai@i-defense.cn>
2019-01-02 15:10:38 +0800

    更新andriod工程

--- App.Andriod/Properties/AssemblyInfo.cs
+++ App.Andriod/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SocketAndriodApp")]
+[assembly: AssemblyTitle("App.Andriod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SocketAndriodApp")]
+[assembly: AssemblyProduct("App.Andriod")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
